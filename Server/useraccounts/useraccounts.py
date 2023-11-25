303f318208b82f92e4d125b4b2728a6918352597
Binary12Spy <134235374+Binary12Spy@users.noreply.github.com>
2023-11-24 21:45:13 -0500

    Removed redundant function

--- Server/useraccounts/useraccounts.py
+++ Server/useraccounts/useraccounts.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 import db
 from useraccounts.authentication import hash_password
 
+#region UserAccount Management
 def register_user_account(username, password):
     users = db.get_user_accounts()
     for user in users:
@@ -19,7 +20,7 @@ def update_user_account(id, updated_user):
         user.passphrase = hash_password(updated_user.password)
     user.color_primary = updated_user.color_primary
     user.color_secondary = updated_user.color_secondary
-    patch_user_account(user)
+    db.patch_user_account(user)
     return True
 
 def delete_user_account(user):
@@ -36,21 +37,10 @@ def get_user_account(user_id):
         return False
     return {"id": user.id, "username": user.username, "primary_color": user.color_primary, "secondary_color": user.color_secondary}
 
-def patch_user_account(user_id, updated_user):
-    user = db.get_user_account_by_id(user_id)
-    if not user:
-        return False
-    user.username = updated_user.username
-    if updated_user.password:
-        user.passphrase = hash_password(updated_user.password)
-    user.color_primary = updated_user.color_primary
-    user.color_secondary = updated_user.color_secondary
-    db.patch_user_account(user)
-    return True
-
 def get_user_accounts():
     users = db.get_user_accounts()
     return [{"id": user.id, "username": user.username, "primary_color": user.color_primary, "secondary_color": user.color_secondary} for user in users]
+#endregion
 
 def is_admin(user_id):
     user = db.get_user_account_by_id(user_id)
